9c0d4dea13f218c220ee46033e934e29fa292f5b
oloco2792 <oloco.github@gmail.com>
2025-08-14 09:57:17 -0400

    Seh

--- pruebas/login.php
+++ pruebas/login.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/styles.css">
-    <title>Login</title>
+    <title>Documento</title>
 </head>
 <body>
+    <form class="form__loginPRUEBA">
+		<label for="user">Usuario</label>
+		<input type="text" name="user">
+		<label for="user">Contraseña</label>
+		<input type="text" name="pass">
+        <input type="submit">
+	</form>
     <div></div>
 </body>
 </html>
